8b5fe6b47f3d40114af6453752996072864d6b7c
Kristen Alexander <kristendelane@gmail.com>
2024-09-17 13:57:06 -0600

    game intro

---
+++ main.py
@@ -0,0 +1,8 @@
+def user_prompt(text):
+    print(text)
+    user_input = input("What do you want to do?")
+    print(user_input)
+
+text = "You are in the emergency room at the local hospital. Thank heavens it is a weekend so you are taken back to a bed fairly quickly and avoid the usual 5 hour wait - you are in an immense amount of pain from a chronic health condition and already know that you need to have a procedure done. You will likely be admitted. You explain you symptoms to the nurse and eventually the doctor comes in to see you. The doctor pulls up your details on the computer and says that the ER treatment plan is to offer you benadryl and another sedative. Although you expected this, you are horrified to know that, rather than treat your pain, they're offering medications to simply knock you out. You are enraged but do everything you can to remain calm because you do not want to be labeled as \"drug seeking\" and know the staff will not take you seriously if you lose your temper. You are accompanied by your partner and you best friend who have offered to advocate for you. You can (a) say nothing and take whatever drugs or tests the medical staff eventuall order or, (b) allow yourself to get angry allow yourself to lose your temper (you have been through this so many times before and are sick of not being listened to or taken seriously), or (c) Ask your friend and partner to advocate for you and keep your own mouth shut (while writhing in pain)"
+
+user_prompt(text)
